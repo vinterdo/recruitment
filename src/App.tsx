d5ee17b97bafb35ac683b74c20ef1d93cad0d878
Vinterdo <pio4426@gmail.com>
2022-11-28 08:35:15 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+import {Flex, Table, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
+import axios from 'axios';
+
+// https://cataas.com/doc.html
+
+const getRandomCat = async () => {
+    return await axios.get('https://cataas.com/cat?json=true')
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const getTags = () => {
+    axios.get('https://cataas.com/api/tags')
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const getCatsByTag = (tag: string) => {
+    axios.get(`https://cataas.com/cat/${tag}`)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const imgRef = useRef<HTMLImageElement>(null);
+
+    useEffect(() => {
+        getRandomCat().then(data => {
+            if(imgRef.current) {
+                console.log(data);
+                imgRef.current.src = `https://cataas.com/${data.url}`;
+            }
+        });
+    });
+
+    return (
+        <Flex>
+            <Flex width="200px">
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th>Tag</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td>Tag</Td>
+                    </Tr>
+                </Tbody>
+            </Table>
+            </Flex>
+            <img ref={imgRef}/>
+        </Flex>
+    );
 }
 
 export default App;
